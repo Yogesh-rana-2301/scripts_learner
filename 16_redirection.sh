1d5535804363db862b09aa14032e7129682c4700
Yogesh-rana-2301 <yogeshrana2301@gmail.com>
2025-08-09 23:25:33 +0530

    Add shell scripts for redirection, /dev/null, and script name
    Added 16_redirection.sh to demonstrate output redirection, 17_devnull.sh to show redirecting output to /dev/null, and 18_nameofscript.sh to print the script name and log a message using logger.

---
+++ 16_redirection.sh
@@ -0,0 +1,9 @@
+#redirection be done through 
+ls > all_files.txt
+
+#transfer all the content from the ls to the file all_file
+
+# if i do another operate like date > , then all prev lost 
+# to have the old plus new do >>
+
+
---
+++ 18_nameofscript.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+echo "Name of this script is ${0}"
+
+
+
+#log 
+#! /bin/bash
+#example of logging
+logger "This is log from ${0}".
